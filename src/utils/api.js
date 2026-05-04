a5b71c98d19edad301afd0eec509327070c30556
Manojkumar M <manojmohan2410@gmail.com>
2026-05-04 17:52:47 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:3000",
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
 })
