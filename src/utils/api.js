ee3ea49c6f14859bb784dc4ba65226a0cc6a6261
Manojkumar M <manojmohan2410@gmail.com>
2026-05-04 17:50:08 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: "http://localhost:8000",
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
 })
